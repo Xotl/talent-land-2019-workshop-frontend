441585dea586d3f07742c221091ed20f4600b1a5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-24 00:03:51 +0000

    Deploy Xotl/talent-land-2019-workshop-frontend to github.com/Xotl/talent-land-2019-workshop-frontend.git:gh-pages

---
+++ precache-manifest.dc8239a5c9ef8a750aca77372d5bf47d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d5eaa6634fc8486cad1f",
+    "url": "/static/js/main.40c33f87.chunk.js"
+  },
+  {
+    "revision": "ed2eb8a31b3d3fe961a8",
+    "url": "/static/js/2.1efa75b7.chunk.js"
+  },
+  {
+    "revision": "d5eaa6634fc8486cad1f",
+    "url": "/static/css/main.6e48fadf.chunk.css"
+  },
+  {
+    "revision": "27c0d69a905264e0aa4190311dbb1425",
+    "url": "/index.html"
+  }
+];
